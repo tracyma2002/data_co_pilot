2718721c87180256f5ca34d0edf3c417c01d5611
李靖雯123 <1054439720@qq.com>
2024-06-08 15:55:17 +0800

    rac

--- data_co_pilot/urls.py
+++ data_co_pilot/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('home/', data_pilot.views.home),
     path('home/search/',data_pilot.views.search),
     #path('api/nl2sql/', data_pilot.views.natural_language_to_sql_view, name='nl2sql'),
-    #path('home/search/sql/',data_pilot.views.add_query),
-    path('query/',data_pilot.views.add_query)
+    path('home/search/sql/',data_pilot.views.add_query),
+    #path('query/',data_pilot.views.add_query)
 ]
