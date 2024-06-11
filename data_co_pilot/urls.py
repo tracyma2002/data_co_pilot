3829130397772c944dc99aeb56c4c3e25b50788e
李靖雯123 <1054439720@qq.com>
2024-06-11 22:57:26 +0800

    rac

--- data_co_pilot/urls.py
+++ data_co_pilot/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     # 将根 URL 指向包含登录表单的 index 视图
     path('', data_pilot.views.index, name='index'),
     #path('login/', data_pilot.views.myhome, name='login'),
-    path('home/', data_pilot.views.home),
+    #path('home/', data_pilot.views.home),
     path('home/search/',data_pilot.views.search),
     #path('api/nl2sql/', data_pilot.views.natural_language_to_sql_view, name='nl2sql'),
     path('home/search/sql/',data_pilot.views.add_query,name='add_query'),
